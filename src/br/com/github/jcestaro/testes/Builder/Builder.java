ef7f17834ece25fd315d572b7aa42997c8a9e85a
jcestaro <joseaugusto.conceicao@bluesoft.com.br>
2019-04-15 18:11:45 -0300

    #185320-aplicando-builder

--- src/br/com/github/jcestaro/testes/Builder/Builder.java
+++ src/br/com/github/jcestaro/testes/Builder/Builder.java
@@ -3,6 +3,14 @@ package br.com.github.jcestaro.testes.Builder;
 public class Builder {
 
 	public static void main(String[] args) {
+		Lanche lanche = new Lanche.Builder("Brioche")
+				.hamburguer(2)
+				.queijo(true)
+				.tomate(true)
+				.bacon(true)
+				.alface(false)
+				.build();
 
+		System.out.println(lanche);
 	}
 }
